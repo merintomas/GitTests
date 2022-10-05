058fe3d5a038e8206ae9d98c6e82f5a87f97d22e
Merin Thomas <merintomas7@gmail.com>
2022-10-05 22:57:44 +0530

    Added a new title check

--- AmazonLaunch.java
+++ AmazonLaunch.java
@@ -9,12 +9,24 @@ public class AmazonLaunch {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 System.setProperty("webdriver.chrome.driver","D:\\Drivers\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","D:\\Drivers\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.amazon.in/");
+		System.out.println("Launching amazon");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		String actualTitle=driver.getTitle();
+		if(actualTitle.equals("Amazon"))
+		{
+			System.out.println("Titles are matching");
+		}
+		else
+		{
+			System.out.println("titles are not matching");
+		}
 		
+		driver.navigate().back();
+		driver.close();
 	}
 
 }
